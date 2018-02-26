ff330efb7ee1a0161b4829ba307549ec31ff4081
whunting <442393660@qq.com>
2018-02-26 18:05:23 +0800

    ListView Adapter #01

--- app/src/main/java/com/example/hunting/yike/MainActivity.java
+++ app/src/main/java/com/example/hunting/yike/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         AVOSCloud.setDebugLogEnabled(true);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> foodAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, Food.getALLFoods());
+        ArrayAdapter<Food> foodAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Food.getALLFoods());
         ListView listView = findViewById(R.id.food_listView);
         listView.setAdapter(foodAdapter);
 
